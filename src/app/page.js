996c428ec8935c6f90efa8aa8fb0442b07bb3aa7
Jabir <jabir.codescriptors@gmail.com>
2026-08-18 13:21:37 +0600

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -472,6 +472,7 @@ export default function LandingPage() {
                 { name: 'Nobodik News', light: '/partners/nobodik white.png', dark: '/partners/nobodik dark.png' },
                 { name: 'Venfyy', light: '/partners/venfyy white.png', dark: '/partners/venfyy dark.png' },
                 { name: 'Tea Bondhu', light: '/partners/tea white.png', dark: '/partners/tea dark.png' },
+                { name: 'London Educators', light: '/partners/le white.png', dark: '/partners/le dark.png' },
               ]).map((partner, i) => (
                 <div key={i} className="eh-marquee-item">
                   <img
